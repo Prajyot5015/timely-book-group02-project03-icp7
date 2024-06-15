805eafa419f64b70bf1ce3dc9df038973c066656
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-06-16 03:08:37 +0530

    Created Movie card and json data file

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './views/Home/Home';
-import About from './views/About/About';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-<>
-{/* <Home /> */}
-<About />
-</>
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />
+    }
+])
 
-);
+
+root.render(<RouterProvider router={router} />);
 
 
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import './Home.css'
+import moviesData from "./../../data"
+import MovieCard from '../../components/MovieCard/MovieCard'
 
 function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <div className='movie-container'>
+        {
+          moviesData.map((movieObject, i) => {
+            const {
+              id,
+              title,
+              description,
+              duration,
+              genre,
+              rating,
+              langauage,
+              poster
+            } = movieObject
+
+            return (
+              <MovieCard
+                key={i}
+                id={id}
+                title={title}
+                description={description}
+                duration={duration}
+                rating={rating}
+                langauage={langauage}
+                genre={genre}
+                poster={poster}
+              />
+            )
+          })
+        }
+      </div>
+    </>
   )
 }
 
