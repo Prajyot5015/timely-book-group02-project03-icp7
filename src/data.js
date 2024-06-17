05fde605439f82a41f0fc2069bd306c66084efac
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-06-17 15:48:19 +0530

    Modified Movie data

--- src/data.js
+++ src/data.js
@@ -8,86 +8,187 @@ import DuneImg from './imgs/Dune.jpeg'
 import NoTimeToDieImg from './imgs/No-Time-to-Die.jpeg'
 import BlackWidowImg from './imgs/Black-Widow.jpeg'
 
-const moviesData =[
+
+const moviesData=[
           {
             id: 1,
             title: "Sangharshyoddha Manoj Jarange Patil",
-            description: "The film is based on the life story of Maratha activist Manoj Jarange Patil.",
+            description: "The film is based on the life story of Maratha activist Manoj Jarange Patil. Sangharsh Yoddha Manoj Jarange Patil is an Indian Marathi-language biographical film centered on the life of Manoj Jarange Patil, a prominent activist for Maratha quota rights. Directed by Shivaji Doltade and produced by Govardhan Doltade under Sonai Films Creation, the film features Rohan Patil in the titular role.",
             duration: "2h 29m",
             genre: ["Biography" , "Drama"],
             rating : "5.7",
             langauage : "Marathi",
-            poster: SangharshYoddhaImg
-          },
+            poster: SangharshYoddhaImg, 
+
+
+             Director:"Shivaji Doltade",
+              Actor:" Rohan Patil",
+              Writer:"Radha",
+              Screenpla:"Sudhir Nikam",
+              Story :	"Govardhan Doltade",
+              Produced:"Govardhan Doltade",
+             Starring:"Rohan Patil,Mohan Joshi,Sandeep Pathak,Gayathri Jadhav",
+             Cinematography:"	Krishna Naikar",
+              Production:"Sonai Films Creation",
+              Releasedate:"14 June 2024",
+             
+              avtar:"https://i.pravatar.cc/150?img=28",
+           },
+
           {
             id: 2,
             title: "Naach Ga Ghuma",
-            description: "Naach Ga Ghuma follows the lively duo - the woman of the house and the house help, and how their love-hate relationship unfolds in a humorous drama.",
+            description: "Naach Ga Ghuma follows the lively duo - the woman of the house and the house help, and how their love-hate relationship unfolds in a humorous drama. is everyday life story of ordinary people turned into an extraordinary art! The premise is simple but the dialogues, direction, acting, comedy, drama & production quality is top notch. Story has heart, it will make you laugh, it will make you cry, it will make you think.",
             duration: "2h 19m",
             genre: ["Comedy","Drama","Family"],
             rating : "8.5",
             langauage : "Marathi",
-            poster: NachGaGhumaImg
+            poster: NachGaGhumaImg,
+
+
+            Director:"Paresh Mokashi.",
+            Actor:"Mukta Barve",
+            Screenpla:"Namrata Awte",
+            Story :	"Govardhan Doltade",
+            Produced:"Swapnil Joshi",
+             Starring:" Sunil Abhyankar, Namrata Awate Sambherao,Ramdas Jadhav",
+             Cinematography:"	Krishna Naikar",
+              Production:" Hiranyagarbha Manoranjan.",
+              Releasedate:"1 May 2024",
+              avtar:"https://i.pravatar.cc/150?img=19",
+            
+           
           },
           {
-            id: 3,
+            id:3,
             title: "K.G.F 2",
-            description: "The blood-soaked land of Kolar Gold Fields (KGF) has a new overlord now - Rocky, whose name strikes fear in the heart of his foes. His allies look up to Rocky as their saviour, the government sees him as a threat to law and order; enemies are clamouring for revenge and conspiring for his downfall. Bloodier battles and darker days await as Rocky continues on his quest for unchallenged supremacy.",
+            description: "The blood-soaked land of Kolar Gold Fields (KGF) has a new overlord now - Rocky, whose name strikes fear in the heart of his foes. His allies look up to Rocky as their saviour, the government sees him as a threat to law and order; enemies are clamouring for revenge and conspiring for his downfall. Bloodier battles and darker days await as Rocky continues on his quest for unchallenged supremacy.n Short. Kolar is a city in Karnataka, famous for gold mines. The story of KGF, set in Kolar Gold Fields, was however, fictional. Kolar Gold Fields was shut down in 2001.",
             duration: 142,
             genre: ["Action"],
             rating : "9.3",
             langauage : "Hindi",
-            poster: KgfImg
+            poster: KgfImg,  
+            Director:"Prashanth Neel",
+              Actor:" Sanjay Dutt, Raveena Tandon, Srinidhi",
+              Screenpla:"Sudhir Nikam",
+              Story :	"Govardhan Doltade",
+              Produced:"Vijay Kiragandur",
+              Starring:"Easwari Rao, John Kokken, T.S Nagabharana and Malavika Avinash.",
+              Cinematography:"	Bhuvan Gowda",
+              Production:"Hombale Films.",
+              Releasedate:" 14 April 2022 ",
+              avtar:"https://i.pravatar.cc/150?img=28",
+         
           } ,
           {
             id: 4,
             title: "Avatar: The Way of Water",
-            description: "Jake Sully lives with his newfound family formed on the planet of Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Na'vi race to protect their home.",
+            description: "Jake Sully lives with his newfound family formed on the planet of Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Na'vi race to protect their home.Sam Worthington · Jake ; Zoe Saldana · Neytiri ; Sigourney Weaver · Kiri ;It is the first installment in the Avatar film series. It is set in the mid-22nd century, when humans are colonizing Pandora, a lush habitable moon of a gas.A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
             duration: "3h 10m",
             genre: ["Action", "Adventure", "Sci-Fi"],
             rating: "7.9",
             langauage: "English",
             poster: Avatar2Img,
+
+             Director:"James Cameron",
+              Actor:"Sam Worthington · Jake ; Zoe Saldana · Neytiri ; Sigourney Weaver · Kiri ;",
+             Screenpla:"Sudhir Nikam",
+              Story :	"James Cameron",
+              Produced:"jon landau",
+             Starring:"Sam Worthington · Jake ; Zoe Saldana · Neytiri ; Sigourney Weaver · Kiri ;",
+             Cinematography:"	Krishna Naikar",
+              Production:"Sonai Films Creation",
+              Releasedate:"December 6, 2022,",
+              avtar:"https://i.pravatar.cc/150?img=21",
+        
           },
           {
             id: 5,
             title: "Top Gun: Maverick",
-            description: "After more than thirty years of service as one of the Navy's top aviators, Pete Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancement in rank that would ground him.",
+            description: "After more than thirty years of service as one of the Navy's top aviators, Pete Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancement in rank that would ground him.After thirty years, Maverick is still pushing the envelope as a top naval aviator, but must confront ghosts of his past when he leads TOP GUN's elite graduates on a mission that demands the ultimate sacrifice from those chosen to fly it.",
             duration: "2h 11m",
             genre: ["Action", "Drama"],
             rating: "8.4",
             langauage: "English",
             poster: TopGunMaverickImg,
+          
+              Director:":Maverick Director",
+              Actor:" Tom Cruise · Capt. Pete 'Maverick' Mitchel,Penny Benjamin ,Bradley 'Rooster' Bradshaw ; Val Kilmer  ",
+              Screenpla:"Sudhir Nikam",
+              Story :	"Govardhan Doltade",
+              Produced:"Columbia Pictures and Marvel .",
+             Starring:"Rohan Patil,Mohan JoshiGayathri Jadhav",
+             Cinematography:"Mauro Fiore",
+              Production:" Columbia Pictures and Marvel ",
+              Releasedate:"  December 13, 2021",
+              avtar:"https://i.pravatar.cc/150?img=20",
+          
           },
           {
             id: 6,
             title: "Spider-Man: No Way Home",
-            description: "With Spider-Man's identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man.",
+            description: "With Spider-Man's identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man.Spider-Man: No Way Home is a 2021 American superhero film based on the Marvel Comics character Spider-Man, co-produced by Columbia Pictures and Marvel .",
             duration: "2h 28m",
             genre: ["Action", "Adventure", "Sci-Fi"],
             rating: "8.7",
             langauage: "English",
             poster: SpiderManNoWayHomeImg,
+          
+              Director:"Jon Watts",
+              Actor:"Tom Holland, Zendaya,Benedict, Ned Leeds,Jon Favreau , Happy Hogan.",
+              Screenpla:"Sudhir Nikam",
+              Story :	"Chris McKenna and Erik Sommers.",
+              Produced:"Govardhan Doltade",
+             Starring:"Rohan Patil,Mohan Joshi,Sandeep Pathak,Gayathri Jadhav",
+             Cinematography:"	Mauro Fiorer",
+              Production:"Columbia Pictures and Marvel",
+              Releasedate:"December 13, 2021",
+              avtar:"https://i.pravatar.cc/150?img=19",
+           
           },
           {
             id: 7,
             title: "Dune",
-            description: "Paul Atreides leads nomadic tribes in a battle to control the desert planet Arrakis.",
+            description: "Paul Atreides leads nomadic tribes in a battle to control the desert planet Arrakis.Dune (titled onscreen as Dune: Part One) is a 2021 American epic science fiction film directed and co-produced by Denis Villeneuve, who co-wrote the screenplay with Jon Spaihts, and Eric Roth. It is the first of a two-part adaptation of the 1965 novel of the same name by Frank Herbert.",
             duration: "2h 35m",
             genre: ["Adventure", "Drama", "Sci-Fi"],
             rating: "8.2",
             langauage: "English",
             poster: DuneImg,
+           
+              Director:" Denis Villeneuve",
+              Actor:"Timothée Chalamet, Oscar Isaac, Josh Brolin, ",
+             Screenpla:"Sudhir Nikam",
+              Story :	"Frank Herbert",
+              Produced:"Govardhan Doltade",
+             Starring:"Stellan Skarsgård,  Stephen McKinley Henderson",
+             Cinematography:"	Krishna Naikar",
+              Production:"Sonai Films Creation",
+              Releasedate:" October 22, 2021",
+              avtar:"https://i.pravatar.cc/150?img=28",
+        
           },
           {
             id: 8,
             title: "No Time to Die",
-            description: "James Bond has left active service. His peace is short-lived when Felix Leiter, an old friend from the CIA, turns up asking for help, leading Bond onto the trail of a mysterious villain armed with dangerous new technology.",
+            description: "James Bond has left active service. His peace is short-lived when Felix Leiter, an old friend from the CIA, turns up asking for help, leading Bond onto the trail of a mysterious villain armed with dangerous new technology.After being delayed by Boyle's departure and later by the COVID-19 pandemic, No Time to Die premiered at the Royal Albert Hall in London on 28 September 2021. It was theatrically released on 30 September 2021 in the United Kingdom and on 8 October 2021 in the United States.",
             duration: "2h 43m",
             genre: ["Action", "Adventure", "Thriller"],
             rating: "7.3",
             langauage: "English",
             poster: NoTimeToDieImg,
+           
+              Director:"James Bond",
+              Actor:"Ben Whishaw · Naomie Harris · Jeffrey Wright · Christoph Waltz · Ralph",
+              Screenpla:"Sudhir Nikam",
+              Story :	"Govardhan Doltade",
+              Produced:"Cary Joji Fukunaga",
+             Starring:" Starring. Daniel Craig · Rami Malek · Léa Seydoux · Lashana Lynch",
+             Cinematography:"	Krishna Naikar",
+              Production:"Sonai Films Creation",
+              Releasedate:" 30 September 2021",
+              avtar:"https://i.pravatar.cc/150?img=21",
+          
           },
           {
             id: 9,
@@ -98,6 +199,18 @@ const moviesData =[
             rating: "6.8",
             langauage: "English",
             poster: BlackWidowImg,
+           
+              Director:"smita",
+              Actor:"ram",
+              Screenpla:"Sudhir Nikam",
+              Story :	"Govardhan Doltade",
+              Produced:"Govardhan Doltade",
+             Starring:"Rohan Patil,Mohan Joshi,Sandeep Pathak,",
+             Cinematography:"	Krishna Naikar",
+              Production:"Sonai Films Creation",
+              Releasedate:"14 June 2024",
+              avtar:"https://i.pravatar.cc/150?img=20",
+          
           }
       
 ]
