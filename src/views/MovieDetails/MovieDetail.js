cd041bdfd4d244f3121108ef1dfbf41995cfbfcb
NikitaNimse <nimsenikita710>
2024-06-17 14:40:55 +0530

    Folder created

---
+++ src/views/MovieDetails/MovieDetail.js
@@ -0,0 +1,19 @@
+
+import { useParams } from "react-router-dom";
+import  moviesData from "../../data";
+import './MovieDetail.css';
+import StarPng from'../../components/MovieCard/star.png';
+
+function MovieDetails() {
+
+    const {id} = useParams()
+   
+    const selectedMovie = moviesData.find((movieObject)=> movieObject.id === id)
+   
+      
+    return(<>
+
+    </>
+    )
+}
+export default MovieDetails
