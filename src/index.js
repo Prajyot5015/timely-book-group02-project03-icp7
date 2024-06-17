d95a479645701e1e5d63d1508bc09104fca5cf19
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-06-17 16:14:04 +0530

    Modified index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import About from './views/About/About'
 import LoginForm from './components/LoginForm/login';
 import FoodList from './components/FoodList/FoodList';
 import Food from './views/Food/Food';
+import AboutCardPage from './views/AboutCardPage/AboutCardPage';
+import FoodCardPage from './views/FoodCardPage/FoodCardPage';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 
@@ -30,8 +32,16 @@ const router = createBrowserRouter([
         element: <FoodList />
     },
     {
-        path : '/food',
+        path: '/food',
         element: <Food />
+    },
+    {
+        path: "/aboutcardpage/:id",
+        element: <AboutCardPage />
+    },
+    {
+        path: "/aboutfoodcardpage/:id",
+        element: <FoodCardPage />
     }
 ])
 
