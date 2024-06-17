b99bbd3640554917f588ce8adad7d2c8076ff9b3
MaheshwariButle07 <maheshwaributle07@gmail.com>
2024-06-17 11:07:51 +0530

    pulling request

--- src/components/AboutUsCard/AboutUsCard.js
+++ src/components/AboutUsCard/AboutUsCard.js
@@ -2,15 +2,15 @@ import "./AboutUsCard.css"
 
 
 
-function AboutUsCard({id,image,title}) {
+function AboutUsCard({ id, image, title }) {
   return (
-    
+
     <>
-   
-   <div className="card-container">
-        <img src={image} className="card-image"/>
+
+      <div className="card-container">
+        <img src={image} className="card-image" />
         <span className="title">{title}</span>
-    </div>
+      </div>
     </>
   )
 }
