b4991cf9a94fc7a4ec1ade67658953281139ce4f
sauravshete2003 <sauravshete72@gmail.com>
2024-06-17 17:28:45 +0530

    add food home page

--- src/components/FoodCard/FoodCard.js
+++ src/components/FoodCard/FoodCard.js
@@ -1,16 +1,18 @@
 import "./FoodCard.css"
 import "../../FoodData"
-
+import CardRating from "../CardRating/CardRating";
 // import { Link } from "react-router-dom";
 
-function FoodCard({ name, image, price,  description}) {
+function FoodCard({ name, image,  description, rating, totalstars = 5 }) {
     return (
         <div className="food-card">
             <img src={image} alt={name} className="food-card-image" />
             <div className="food-card-content">
                 <h3 className="food-card-name">{name}</h3>
                 <p className="food-description">{description.substring(0,80)}</p>
-                <p className="food-card-price">${price}</p>
+                <span className="food-rating">
+                    <CardRating rating={rating} totalstars={totalstars} />
+                </span>
             </div>
             <button type="buttom" className="food-card-btn" >Buy Now</button>
             <button type="buttom" className="food-card-btn" >View Details</button>
