6d9eaa39e92ce622e1d69b07681e0170ec92f8a5
NikitaNimse <nimsenikita710>
2024-06-17 14:49:06 +0530

    information Added

--- src/views/MovieDetails/MovieDetail.js
+++ src/views/MovieDetails/MovieDetail.js
@@ -13,6 +13,27 @@ function MovieDetails() {
       
     return(<>
    <h1>Movie Detail</h1>
+   <img src={selectedMovie.poster/10} alt="'poster"  className='poster-img'/>
+       
+       <img src={StarPng} alt='img' />
+       <p>{selectedMovie.rating} </p>
+        
+       
+       <span > {selectedMovie.author.description}</span>
+       <p>{selectedMovie.duration} </p>
+       <p>{selectedMovie.genre} </p>
+      
+       <p >{selectedMovie.langauage} </p>
+       <p >{selectedMovie.Director} </p>
+       <p >{selectedMovie.Actor} </p>
+       <p >{selectedMovie.Screenpla} </p>
+       <p >{selectedMovie.Story } </p>
+       <p >{selectedMovie.Produced} </p>
+       <p >{selectedMovie.Starring} </p>
+       <p >{selectedMovie.Cinematography} </p>
+       <p >{selectedMovie.Production} </p>
+       <p >{selectedMovie.Releasedate} </p>
+       <p >{selectedMovie.avtar} </p>
     </>
     )
 }
